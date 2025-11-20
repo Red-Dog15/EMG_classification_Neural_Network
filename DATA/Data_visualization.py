ea0c71fe1b4d31e1c63e98588deadabd97c617a3
Red-Dog15 <nicholascweng@gmail.com>
2025-11-20 21:58:59 +0000

    edited results files for example data

--- DATA/Data_visualization.py
+++ DATA/Data_visualization.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import torch
 import os
-
+import Data_Conversion as DC
 
 def plot_window(window, file_name):
     # window shape: (seq_len, channels)
@@ -17,7 +17,16 @@ def plot_window(window, file_name):
     plt.close()
     plt.show()
     
-
 # Ensure folder exists
 os.makedirs("DATA/results", exist_ok=True)
 plot_window(10 * torch.rand(100, 8), "random_generated_data")  # example usage with random data
+
+plot_window(DC.tensor_Light_Hand_Open, "Low_intensity_data")  # example usage tensor Light movements
+plot_window(DC.tensor_Medium_Hand_Open, "Medium_intensity_data")  # example usage tensor Medium movements
+plot_window(DC.tensor_Hard_Hand_Open, "Hard_intensity_data") # example usage tensor Hard movements
+"""
+# 
+plot_window(DC.tensors_dict["Light"][0][7], "Low_intensity_data")  # example usage tensor Light movements
+plot_window(DC.tensors_dict["Medium"][0][7], "Medium_intensity_data")  # example usage tensor Medium movements
+plot_window(DC.tensors_dict["Hard"][0][7], "Hard_intensity_data") # example usage tensor Hard movements
+"""
