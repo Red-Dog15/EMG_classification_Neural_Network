900d8c27f24d36a0219e9ef732e9748c1fa0aa40
Red-Dog15 <nicholascweng@gmail.com>
2025-11-20 16:24:08 +0000

    working tenors and import to main

--- DATA/Data_Conversion.py
+++ DATA/Data_Conversion.py
@@ -1,11 +1,8 @@
 """ Example Data : https://github.com/LibEMG/ContractionIntensity/blob/main/Info.txt"""
-import os
-print(os.path.abspath("./example_data/S1_Hard_C1_R1.csv"))
 # Convert CSV to DATAFRAMES
 import pandas as pd
 
 # assign variables for CSV paths of example movements
-
 CSV_No_Movement = r"./data/example_data/S1_Hard_C1_R1.csv"
 CSV_Wrist_Flexion = r"./data/example_data/S1_Hard_C2_R1.csv"
 CSV_Wrist_Extension = r"./data/example_data/S1_Hard_C3_R1.csv"
@@ -26,3 +23,20 @@ df_Chuck_Grip = pd.read_csv(CSV_Chuck_Grip)
 df_Hand_Open = pd.read_csv(CSV_Hand_Open)
 
 print ("dataframes Loaded")
+
+# create Pytorch tensor datasets equivalents
+from torch import tensor
+
+print ("Tensors Loading...")
+tensor_No_movement = tensor(df_No_movement.values)
+tensor_Wrist_Flexion = tensor(df_Wrist_Flexion.values)
+tensor_Wrist_Extension = tensor(df_Wrist_Extension.values)
+tensor_Wrist_Pronation = tensor(df_Wrist_Pronation.values)
+tensor_Wrist_Supination = tensor(df_Wrist_Supination.values)
+tensor_Chuck_Grip = tensor(df_Chuck_Grip.values)
+tensor_Hand_Open = tensor(df_Hand_Open.values)
+print ("Tensors Loaded")
+
+tensors_list = [tensor_No_movement, tensor_Wrist_Flexion, tensor_Wrist_Extension,
+                tensor_Wrist_Pronation, tensor_Wrist_Supination, tensor_Chuck_Grip,
+                tensor_Hand_Open]   
--- NN/main.py
+++ NN/main.py
@@ -1,8 +1,11 @@
+import os, sys
+
+# Add parent directory of this file (Scripts/) to module search path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 import torch
 import torch.nn as nn
-import pandas as pd
+from DATA.Data_Conversion import tensors_list
 
+print (tensors_list[1])
 
-x = torch.rand(5, 3)
-print(x)
