eae34fbe4bc064f02649d67ef1e33cd3ee4cbe05
Red-Dog15 <nicholascweng@gmail.com>
2026-01-02 19:15:09 +0000

    added template functions for Data_Mapping, in this case for myosuite conversion

--- DATA/Data_Mapping.py
+++ DATA/Data_Mapping.py
@@ -3,25 +3,45 @@ Docstring for Scripts.DATA.Data_Mapping
 
 this module provides mapping utilities for EMG data classification.
 """
-open
+
 # Mapping utilities for EMG data
 
-def convert_data(file):
+def NN_data_parser(file):
     """
     Docstring for parse_data
     
-    :param file: Description
+    :param file: File for data parsing
     """
-    with open(output_file, "r") as f:
+    with open(file, "r") as f:
         data = f.read()
         f.close()
     print(data)
+    return data
 
+def Severity_Converter(severity_level, max_severity=5):
+    """
+    Converts Severity predictions into intesity multipliers for simulation/hardware.
+    
+    :param severity_level: Severity level (int)
+    :param max_severity: Maximum severity level, change depending on application (int)
+    :return: Scaled severity (float)
+    """
+    if 0 <= severity_level <= max_severity:
+        return severity_level / max_severity
+    else:
+        raise ValueError(f"Expected severity level must be between 0 and {max_severity}.")
+    
+def activation_blender():
+    """
+    combines probability weightings into mutli-muscle movement pattern
+    
+    :param : Description
+    """
+    pass
 
-class myosuite_mapping:
+class Muscle_Mapping:
     def __init__(self):
         pass
-
 
     def Muscle_activation_Index(index):
         """
@@ -29,4 +49,12 @@ class myosuite_mapping:
         
         :param index: Description
         """
-        get_movement_label(0)
+        pass
+        
+    def MyoSuiteFormatter(data):
+        """
+        Converts data to MyoSuite compatible format
+        
+        :param data: data to be formatted, Expects: (instert expected format)
+        """
+        pass
